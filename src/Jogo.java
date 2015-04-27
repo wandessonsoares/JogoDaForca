882fcefc7281009c7de4bcbc5f6c741b472e4387
Wandesson Soares <wandessonsilvatsi@gmail.com>
2015-04-27 20:48:24 -0300

    funcionando

--- src/Jogo.java
+++ src/Jogo.java
@@ -5,11 +5,38 @@ public class Jogo {
 		String palavracorreta = "android";
 		Scanner s = new Scanner(System.in);
 		String chute;
+		int chances = 3;
+		int acertos = 0;
 		
 		StringBuffer palavraTemp = trocaLetras(palavracorreta);
 		
-		System.out.println("Palavra: " + palavraTemp +"\nDigite uma letra:");
-		chute = s.nextLine();
+		while(true){
+			System.out.println("Palavra: " + palavraTemp +"\nDigite uma letra:");
+			chute = s.nextLine();
+			
+			
+			if (temLetra(palavracorreta, chute)) {
+				palavraTemp = colocaLetraCerta(palavracorreta, palavraTemp, chute);
+				acertos++;
+				
+				if(acertos == (palavracorreta.length()-1)){
+					System.out.println("Parabéns! Você acertou!");
+					break;
+				}
+			}
+			else{
+				chances--;
+				System.out.println("A palavra não tem essa letra! Tente novamente... (chances: " + chances + ")");
+				
+				if(chances < 1){
+					System.out.println("Você perdeu! A palavra certa era: " + palavracorreta);
+					break;
+				}
+			}
+			
+		}
+		
+		System.out.println("Fim do jogo.");
 	}
 	
 	public static StringBuffer trocaLetras(String palavra){
@@ -22,11 +49,11 @@ public class Jogo {
 		return trocada;
 	}
 	
-	public static boolean procuraLetra(String palavra, String chute){
+	public static boolean temLetra(String palavra, String chute){
 		int count = 0;
 		
 		for (int i = 0; i < palavra.length(); i++) {
-			if (chute.equals(palavra.charAt(i))) {
+			if (chute.equals(String.valueOf(palavra.charAt(i)))) {
 				count++;
 			}
 		}
@@ -37,5 +64,16 @@ public class Jogo {
 		else{
 			return false;
 		}
+	}
+	
+	public static StringBuffer colocaLetraCerta(String palavracorreta, StringBuffer palavraTemp, String letra){		
+		for (int i = 0; i < palavracorreta.length(); i++) {
+			if(letra.equals(String.valueOf(palavracorreta.charAt(i)))){
+				palavraTemp.deleteCharAt(i);
+				palavraTemp.insert(i, letra);
+			}
+		}
+		
+		return palavraTemp;
 	}
 }
